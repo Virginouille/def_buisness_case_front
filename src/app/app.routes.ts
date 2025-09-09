46b36dc69626de66e901e6fa0fe965606d53d65e
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-09-09 21:53:12 +0200

    rooting home component mis en place

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }, //Si vide, redirection vers home
+    { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent }
 ];
