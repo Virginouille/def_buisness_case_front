e52e07f0ba7086930dd1ccb2df97d54eed6a4c63
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-09-26 15:28:17 +0200

    ajout modale au component card-borne

--- src/app/components/dashboard/card-borne/card-borne.component.ts
+++ src/app/components/dashboard/card-borne/card-borne.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ModifyBorneComponent } from "../modify-borne/modify-borne.component";
 
 @Component({
   selector: 'app-card-borne',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ModifyBorneComponent],
   templateUrl: './card-borne.component.html',
   styleUrl: './card-borne.component.css'
 })
