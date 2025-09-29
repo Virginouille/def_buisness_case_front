0b94679e43437b7f03960b5103f53d3b6e507375
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-09-29 15:25:20 +0200

    avancement structure angular du form

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
 
+  contactForm: FormGroup;
+
+  //Structure du formulaire
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      prenom: ['', [Validators.required, Validators.pattern(/^[a-zA-ZÀ-ÿ\s-]{2,}$/)]],
+      nom: ['', [Validators.required, Validators.pattern(/^[a-zA-ZÀ-ÿ\s-]{2,}$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      telephone: ['', [Validators.required, Validators.pattern(/^(?:[0-9] ?){10}$/)]], //Numéro de téléphone avec possibiliés de . et d'espaces
+      message: ['', Validators.required]
+    });
+  }
+
+  //A la soumission du formulaire
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Formulaire de contact soumis :', this.contactForm.value);
+      //penser à utiliser http cliet pour envoyer au back
+    } else {
+      console.log('Formulaire invalide');
+    }
+  }
 }
+
+//Ajouter une méthode qui filtrer les non numériques pour bloquer l'utilisation des lettres dans le numéro de tél
