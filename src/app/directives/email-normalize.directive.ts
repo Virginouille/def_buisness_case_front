5887655c648573f94955b1e5853a8dd3bac7ae87
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-09-30 12:08:19 +0200

    modification commentaires directives email normalize

--- src/app/directives/email-normalize.directive.ts
+++ src/app/directives/email-normalize.directive.ts
@@ -10,9 +10,9 @@ export class EmailNormalizeDirective {
   constructor(private control: NgControl) { }
 
   /**
-   * Méthode qui normalise l'entrée utilisateur en chaine en minuscles
+   * Méthode qui normalise l'entrée utilisateur en chaine en minuscles et supprime les espaces eb début et fin
    * @param value La chaîne de caractères à normaliser
-   * @returns La chaîne normalisée en minuscules
+   * @returns La chaîne normalisée en minuscules et trim
    */
   private normalize(value: string): string {
     if (!value) return '';
@@ -20,9 +20,12 @@ export class EmailNormalizeDirective {
   }
 
   /**
-  * Méthode qui se charge de transformer la valeur du champ quand l'utilisateur valide le forumlaire
-  * utilise la méthode 'normalize'
- */
+   * Normalise la valeur du FormControl associé à cet input.
+   * À appeler lors de la soumission du formulaire.
+   * 
+   * Utilise la méthode `normalize` pour convertir la chaîne en minuscules et supprimer les espaces superflus.
+   * La mise à jour se fait sans émettre d'événements (`emitEvent: false`).
+   */
   public normalizeControl() {
     const value = this.control.control?.value;
     if (value) {
